5e40e21f0281aadb1ad4db5bfc0a4e0748a0b29e
serhio4smotrov <ser19061995@gmail.com>
2024-10-25 17:45:04 +0300

    initial

---
+++ Module 7_1/module_7_1.py
@@ -0,0 +1,41 @@
+class Product:
+
+    def __init__(self,name,weight,category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        f = open(self.__file_name,'r')
+        list_products = f.read()
+        f.close()
+        return list_products
+
+    def add(self,*products):
+        list_products = self.get_products()
+        file = open(self.__file_name,'a')
+        for product in products:
+            if product.__str__() in list_products:
+                print(f'Продукт {product.name} уже есть в магазине')
+            else:
+                file.write(f'{product}\n')
+        file.close()
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+print(p2) # __str__
+s1.add(p1, p2)
+s1.add(p3)
+print(s1.get_products())
